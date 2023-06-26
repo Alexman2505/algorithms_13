b4f76af5af0d603b2afeae92941ef90e6637d471
Alexman2505 <alexman2505@gmail.com>
2023-06-26 14:33:08 +0300

    поправил замечания по задаче В. номер решения 88566146

--- final/b_efficient_quicksort.py
+++ final/b_efficient_quicksort.py
@@ -1,78 +1,88 @@
-# 88495397
-from typing import List, Tuple
+# 88566146
+from typing import List
 
 
-def compare_items(
-    item1: Tuple[str, int, int], item2: Tuple[str, int, int]
-) -> bool:
-    """Сравнивает два элемента по заданным критериям.
+class Participant:
+    def __init__(self, login: str, tasks: int, penalty: int):
+        """Конструктор класса Participant.
 
-    Элементы сравниваются сначала по второму значению (tasks),
-    затем по третьему значению (penalty),
-    и в случае равенства - по первому значению (login).
+        Принимает на вход логин участника, количество выполненных
+        задач участника, и значение штрафа участника.
+        """
+        self.login = str(login)
+        self.tasks = int(tasks)
+        self.penalty = int(penalty)
+
+    def __str__(self) -> str:
+        """Возвращает строковое представление участника."""
+        return self.login
+
+    def __lt__(self, other: 'Participant') -> bool:
+        """Определяет отношение "меньше" для участников.
+
+        Это перезагруженный магический метод представляет собой
+        оператор сравнения "<" меньше.
+        Участники сортируются сначала по количеству выполненных задач
+        (в убывающем порядке), затем по значению штрафа
+        (в возрастающем порядке),и в случае равенства - по логину
+        (в алфавитном порядке). Возвращается булево значение
+        в зависимости от сравнения.
+        """
+        if self.tasks != other.tasks:
+            return self.tasks > other.tasks
+        if self.penalty != other.penalty:
+            return self.penalty < other.penalty
+        return self.login < other.login
+
+
+def quick_sort(array: List[Participant]) -> None:
+    """Выполняет сортировку списка участников по заданным критериям (in-place).
+
+    Принимает на вход список (массив) участников для сортировки
+    Внутри функция _quick_sort рекурсивно вызывает себя и partition
+    для сортировки массива, используя модификацию алгоритма быстрой сортировки.
     """
-    return (
-        item1[1] > item2[1]
-        or (item1[1] == item2[1] and item1[2] < item2[2])
-        or (
-            item1[1] == item2[1]
-            and item1[2] == item2[2]
-            and item1[0] < item2[0]
-        )
-    )
+
+    def partition(start: int, end: int) -> int:
+        """Выполняет разделение списка на подсписки вокруг опорного элемента.
+
+        Принимает на вход индекс начала и конца подсписка. Возвращает индекс
+        опорного элемента. Используется алгоритм Хоара для разделения списка
+        на две части вокруг опорного элемента.
+        """
+        pivot = array[start]
+        left = start + 1
+        right = end
+        while left <= right:
+            while left <= right and array[right] > pivot:
+                right -= 1
+            while left <= right and array[left] < pivot:
+                left += 1
+            if left <= right:
+                array[left], array[right] = array[right], array[left]
+                left += 1
+                right -= 1
+        array[start], array[right] = array[right], array[start]
+        return right
+
+    def _quick_sort(left: int, right: int) -> None:
+        """Выполняет рекурсивную сортировку списка.
+
+        Принимает на вход индекс начала и конца списка.
+        """
+        if left < right:
+            pivot_index = partition(left, right)
+            _quick_sort(left, pivot_index - 1)
+            _quick_sort(pivot_index + 1, right)
+
+    _quick_sort(0, len(array) - 1)
 
 
-def partition(array: List[Tuple[str, int, int]], start: int, end: int) -> int:
-    """Выполняет разделение массива на подмассивы в соответствии с опорным элементом.
+if __name__ == '__main__':
+    number_of_participants = int(input())
+    participants = [
+        Participant(*input().split()) for _ in range(number_of_participants)
+    ]
 
-    Используется алгоритм Хоара для разделения массива на две части вокруг опорного элемента.
-    Args:
-        array: Массив для разделения.
-        start: Индекс начала подмассива.
-        end: Индекс конца подмассива.
-    Returns:
-        Индекс опорного элемента.
-    """
-    pivot = array[start]
-    left = start + 1
-    right = end
-    while left <= right:
-        while left <= right and compare_items(pivot, array[right]):
-            right -= 1
-        while left <= right and compare_items(array[left], pivot):
-            left += 1
-        if left <= right:
-            array[left], array[right] = array[right], array[left]
-            left += 1
-            right -= 1
-    array[start], array[right] = array[right], array[start]
-    return right
-
-
-def quick_sort(
-    array: List[Tuple[str, int, int]], left: int, right: int
-) -> None:
-    """Выполняет сортировку списка по заданным критериям (in-place).
-
-    Используется модификация алгоритма быстрой сортировки "in-place".
-    Args:
-        array: Список для сортировки.
-        left: Индекс начала списка.
-        right: Индекс конца списка.
-    """
-    if left < right:
-        pivot_index = partition(array, left, right)
-        quick_sort(array, left, pivot_index - 1)
-        quick_sort(array, pivot_index + 1, right)
-
-
-n = int(input())
-participants = []
-for _ in range(n):
-    login, tasks, penalty = input().split()
-    participants.append((str(login), int(tasks), int(penalty)))
-
-quick_sort(participants, 0, n - 1)
-for participant in participants:
-    login, _, _ = participant
-    print(login)
+    quick_sort(participants)
+    print(*participants, sep='\n')
